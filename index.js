5e877c15d7d6b328103a9960113093712cb5e762
Kyle Mathews <mathews.kyle@gmail.com>
2014-12-10 12:55:18 -0800

    Add code + package.json

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = function() {
   var mediaQuery;
   if (typeof window !== "undefined" && window !== null) {
-    mediaQuery = "(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-resolution: 1.5dppx)";
-    if (window.devicePixelRatio > 1) {
+    mediaQuery = "(-webkit-min-device-pixel-ratio: 1.25), (min--moz-device-pixel-ratio: 1.25), (-o-min-device-pixel-ratio: 5/4), (min-resolution: 1.25dppx)";
+    if (window.devicePixelRatio > 1.25) {
       return true;
     }
     if (window.matchMedia && window.matchMedia(mediaQuery).matches) {
